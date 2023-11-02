5fe0a59dfa9a056dd4f55175305eb7a970508ec9
ryokatsu <ryokatsu0719@gmail.com>
2023-11-02 17:05:08 +0900

    fix:ページネーションまで

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,8 +2,7 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
+import { fetchLatestInvoices } from "@/app/lib/data";
 export default async function LatestInvoices({}: {}) {
   const latestInvoices = await fetchLatestInvoices();
   return (
